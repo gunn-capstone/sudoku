23379c11723d1ea97f4e03da68064b33dfab58a5
guyzam1r <55858722+guyzam1r@users.noreply.github.com>
2020-03-05 14:54:24 -0800

    Finished solving module

--- src/Square.java
+++ src/Square.java
@@ -1,60 +1,49 @@
 import java.util.ArrayList;
 
 public class Square {
-	int value, index, rowNum, colNum, bSquareNum;
-	int boxSize = 9;
+	
+	int value;
+	int index;
 	ArrayList<Integer> posNums = new ArrayList<Integer>();
-
-	public Square(int value, int index) {
+	
+	public Square(int value, int index){
 		this.value = value;
 		this.index = index;
-		rowNum = index / boxSize;
-		colNum = index % boxSize;
-		// bSquareNum = bsn;
 	}
-
+	
 	public Square(int index) {
 		this.index = index;
-		rowNum = index / boxSize;
-		colNum = index % boxSize;
-		// bSquareNum = bsn;
-		for (int i = 1; i <= boxSize; i++) {
+		for (int i = 1; i <= 9; i++) {
 			posNums.add(i);
 		}
 	}
-
-	public void setValue(int val) {
-		value = val;
+	
+	public Square(int value, int index, ArrayList<Integer> posNums){
+		this.value = value;
+		this.index = index;
+		this.posNums = posNums;
 	}
-
+	
 	public int getValue() {
 		return value;
 	}
-
+	
 	public int getIndex() {
 		return index;
-	}
-
-	public int getRow() {
-		return rowNum;
-	}
-
-	public int getCol() {
-		return colNum;
-	}
-
-	public int getBigSquare() {
-		return bSquareNum;
 	}
 
 	public ArrayList<Integer> getPosNums() {
 		return posNums;
 	}
-
+	
+	public void rmFromPosNums(int n) {
+		posNums.remove(Integer.valueOf(n));
+	}
+	
 	public boolean isFilled() {
 		return posNums.size() == 0;
 	}
-
+	
 	public void fill() {
 		value = posNums.get(0);
 		posNums.clear();
--- src/Sudoku.java
+++ src/Sudoku.java
@@ -1,72 +1,114 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class Sudoku {
-	Square[] squares;
-	int numCells = 81;
+	
+	int[] startingVals;
+    
+    public Sudoku(int[] a) {
+    	startingVals = a;
+    }
 
-	public Sudoku(int[] arr) {
-		squares = new Square[numCells];
-		for (int i = 0; i < numCells; i++) {
-			if (arr[i] == 0) {
-				squares[i] = new Square(i);
-			} else {
-				squares[i] = new Square(arr[i], i);
-			}
-		}
-	}
-
-	public int[] Solve() {
-		int[] arr = new int[numCells];
-		int emptySquares = numCells;
-		// initialize posNums
+    //helper function; call this one from main
+    public int[] solve() {
+    	//initialize arr; stores values of squares
+    	int[] arr = startingVals;
+    	//initialize emptySquares; stores indices of empty squares 
+    	ArrayList<Integer> emptySquares = new ArrayList<Integer>();
+    	emptySquares = (ArrayList<Integer>) IntStream.rangeClosed(0, 80).boxed().collect(Collectors.toList());
+    	//initialize squares
+    	Square[] squares = new Square[81];
+    	for (int i = 0; i < 81; i++) {
+    		if (arr[i] == 0) {
+    			squares[i] = new Square(i);
+    		} else {
+    			squares[i] = new Square(arr[i], i);
+    		}
+    	}
+    	//initialize posNums
+    	Square cur;
+    	for (int i = 0; i < 81; i++) {
+    		cur = squares[i];
+    		if (cur.isFilled()) {
+    			arr[i] = cur.getValue();
+    			updateSquares(i, squares);
+    			emptySquares.remove(Integer.valueOf(i));
+    		}
+    	}
+    	return solve(arr, emptySquares, squares);
+    }
+    
+    //recursive function
+	public int[] solve(int[] arr, ArrayList<Integer> emptySquares, Square[] squares) {
+    	//while remaining squares:
 		Square cur;
-		for (int i = 0; i < numCells; i++) {
-			cur = squares[i];
-			if (cur.isFilled()) {
-				arr[i] = cur.getValue();
-				updateSquares(i);
-				emptySquares--;
-			}
-		}
-		// while remaining squares:
-		int pes = emptySquares;
-		while (emptySquares > 0) {
-			for (int i = 0; i < numCells; i++) {
-				cur = squares[i];
-				if (!cur.isFilled() && cur.getPosNums().size() == 1) {
-					cur.fill();
-					arr[i] = cur.getValue();
-					updateSquares(i);
-					emptySquares--;
-				}
-			}
-
-		}
+    	while (emptySquares.size() > 0) {
+    		boolean same = true;
+    		for (int i = 0; i < 81; i++) {
+    			cur = squares[i];
+    			if (!cur.isFilled() && cur.getPosNums().size() == 1) {
+    				cur.fill();
+    				arr[i] = cur.getValue();
+    				updateSquares(i, squares);
+    				emptySquares.remove(Integer.valueOf(i));
+    				same = false;
+    			} else if (cur.isFilled() && cur.getValue() == 0) {
+    				int[] err = {};
+    				System.out.println("Wrong guess");
+    				return err;
+    			}
+    		}
+    		if (same) {
+    			System.out.println("Made a guess for " + emptySquares.get(0));
+    			int[] storedArr = arr.clone();
+    			ArrayList<Integer> storedES = (ArrayList<Integer>) emptySquares.clone();
+    			Square[] storedSquares = new Square[81];
+    			for(int i = 0; i < 81; i++) {
+    				storedSquares[i] = new Square(squares[i].getValue(), squares[i].getIndex(), (ArrayList<Integer>) squares[i].getPosNums().clone());
+    			}
+    			Square firstES = squares[emptySquares.get(0)];
+    			firstES.fill();
+    			arr[emptySquares.get(0)] = firstES.getValue();
+    			updateSquares(emptySquares.get(0), squares);
+    			emptySquares.remove(0);   			
+    			int[] sol = solve(arr, emptySquares, squares);
+    			if (sol.length == 81) {
+    				return sol;
+    			} else {
+    				arr = storedArr;
+    				emptySquares = storedES;
+    				squares = storedSquares;
+    				squares[emptySquares.get(0)].getPosNums().remove(0);
+    			}
+    		}
+    	}
 		return arr;
-
-	}
-
-	public void updateSquares(int index) {
-		// for all unfilled squares in row, col, and bigsquare --> remove # of given
-		// index from posnums
-		// row
-		for (int i = index / 9 * 9; i < index / 9 * 9 + 9; i++) {
-			if (!squares[i].isFilled()) {
-				squares[i].getPosNums().remove(Integer.valueOf(squares[index].getValue()));
-			}
-		}
-		// col
-		for (int i = index % 9; i < numCells; i += 9) {
-			if (!squares[i].isFilled()) {
-				squares[i].getPosNums().remove(Integer.valueOf(squares[index].getValue()));
-			}
-		}
-		// bsquare
-		int start = index - (((index / 9) % 3) * 9) - (index % 3);
-		int[] nums = { start, start + 1, start + 2, start + 9, start + 10, start + 11, start + 18, start + 19,
-				start + 20 };
-		for (int i : nums) {
-			if (!squares[i].isFilled()) {
-				squares[i].getPosNums().remove(Integer.valueOf(squares[index].getValue()));
-			}
-		}
-	}
+    	
+    }
+    
+    public void updateSquares(int index, Square[] sqs) {
+    	//for all unfilled squares in row, col, and bigsquare --> remove # of given index from posnums
+    	//row
+    	for (int i = index/9*9; i < index/9*9+9; i++) {
+    		if (!sqs[i].isFilled()) {
+    			sqs[i].rmFromPosNums(sqs[index].getValue());
+    		}
+    	}
+    	//col
+    	for (int i = index % 9; i < 81; i += 9) {
+    		if (!sqs[i].isFilled()) {
+    			sqs[i].rmFromPosNums(sqs[index].getValue());
+    		}
+    	}
+    	//bsquare
+    	int start = index - (((index / 9) % 3) * 9) - (index % 3);
+    	int[] nums = {start, start + 1, start + 2, start + 9, start + 10, start + 11, start + 18, start + 19, start + 20};
+    	for (int i : nums) {
+    		if (!sqs[i].isFilled()) {
+    			sqs[i].rmFromPosNums(sqs[index].getValue());
+    		}
+    	}
+    }
 }
